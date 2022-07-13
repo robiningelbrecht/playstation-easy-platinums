ebb5ee67482054e6e473bd6c5cffd166922880b6
robin <robin@baldwin.be>
2022-07-13 09:47:35 +0200

    simplified sort

--- src/Result/ResultSet.php
+++ src/Result/ResultSet.php
@@ -20,88 +20,88 @@ class ResultSet implements \Countable
 
     public function sort(Sorting $sorting): void
     {
-        if ($sorting->getSortField()->getType() === SortField::TYPE_NUMERIC) {
-            usort(
-                $this->rows,
-                function (Row $a, Row $b) use ($sorting) {
-                    $sortField = $sorting->getSortField();
-                    if ($a->getValueForSortField($sortField) === $b->getValueForSortField($sortField)) {
-                        return 0;
-                    }
-
-                    if ($sorting->getSortDirection() === SortDirection::ASC) {
-                        // We want to push null values to the back of the list.
-                        $aValue = $a->getValueForSortField($sortField) !== null ? $a->getValueForSortField($sortField) : 99999;
-                        $bValue = $b->getValueForSortField($sortField) !== null ? $b->getValueForSortField($sortField) : 99999;
-                        return ($aValue < $bValue) ? -1 : 1;
-                    }
-
-                    // We want to push null values to the back of the list.
-                    $aValue = $a->getValueForSortField($sortField) !== null ? $a->getValueForSortField($sortField) : -99999;
-                    $bValue = $b->getValueForSortField($sortField) !== null ? $b->getValueForSortField($sortField) : -99999;
-                    return ($aValue > $bValue) ? -1 : 1;
-                }
-            );
-
-            return;
-        }
-
-        if ($sorting->getSortField()->getType() === SortField::TYPE_DATE) {
-            usort(
-                $this->rows,
-                function (Row $a, Row $b) use ($sorting) {
-                    $sortField = $sorting->getSortField();
-
-                    $aDate = $a->getValueForSortField($sortField);
-                    $bDate = $b->getValueForSortField($sortField);
-
-                    // Because we imported most of the initial games on the 4th of july,
-                    // We're going to do some funky stuff here.
-                    if ($aDate->format('Y-m-d') === self::INITIAL_IMPORT_DATE && $bDate->format('Y-m-d') !== self::INITIAL_IMPORT_DATE) {
-                        return $sorting->getSortDirection() === SortDirection::ASC ? -1 : 1;
-                    }
-                    if ($aDate->format('Y-m-d') !== self::INITIAL_IMPORT_DATE && $bDate->format('Y-m-d') === self::INITIAL_IMPORT_DATE) {
-                        return $sorting->getSortDirection() === SortDirection::ASC ? 1 : -1;
-                    }
-
-                    if ($aDate->format('Y-m-d') === self::INITIAL_IMPORT_DATE && $bDate->format('Y-m-d') === self::INITIAL_IMPORT_DATE) {
-                        // We will take the ID into account here.
-                        if ($a->getId() === $b->getId()) {
+        switch ($sorting->getSortField()) {
+            case SortField::PRICE:
+            case SortField::POINTS:
+            case SortField::TROPHIES:
+            case SortField::TIME:
+                usort(
+                    $this->rows,
+                    function (Row $a, Row $b) use ($sorting) {
+                        $sortField = $sorting->getSortField();
+                        if ($a->getValueForSortField($sortField) === $b->getValueForSortField($sortField)) {
                             return 0;
                         }
 
                         if ($sorting->getSortDirection() === SortDirection::ASC) {
-                            return ($a->getId() < $b->getId()) ? -1 : 1;
+                            // We want to push null values to the back of the list.
+                            $aValue = $a->getValueForSortField($sortField) !== null ? $a->getValueForSortField($sortField) : 99999;
+                            $bValue = $b->getValueForSortField($sortField) !== null ? $b->getValueForSortField($sortField) : 99999;
+                            return ($aValue < $bValue) ? -1 : 1;
                         }
 
-                        return ($a->getId() > $b->getId()) ? -1 : 1;
+                        // We want to push null values to the back of the list.
+                        $aValue = $a->getValueForSortField($sortField) !== null ? $a->getValueForSortField($sortField) : -99999;
+                        $bValue = $b->getValueForSortField($sortField) !== null ? $b->getValueForSortField($sortField) : -99999;
+                        return ($aValue > $bValue) ? -1 : 1;
                     }
+                );
+                break;
+            case SortField::DATE:
+                usort(
+                    $this->rows,
+                    function (Row $a, Row $b) use ($sorting) {
+                        $sortField = $sorting->getSortField();
 
-                    // Starting from here it's just normal sorting on date.
-                    $aDateValue = strtotime($aDate->format('Y-m-d H:i:s'));
-                    $bDateValue = strtotime($bDate->format('Y-m-d H:i:s'));
-                    if ($sorting->getSortDirection() === SortDirection::ASC) {
-                        return ($aDateValue < $bDateValue) ? -1 : 1;
+                        $aDate = $a->getValueForSortField($sortField);
+                        $bDate = $b->getValueForSortField($sortField);
+
+                        // Because we imported most of the initial games on the 4th of july,
+                        // We're going to do some funky stuff here.
+                        if ($aDate->format('Y-m-d') === self::INITIAL_IMPORT_DATE && $bDate->format('Y-m-d') !== self::INITIAL_IMPORT_DATE) {
+                            return $sorting->getSortDirection() === SortDirection::ASC ? -1 : 1;
+                        }
+                        if ($aDate->format('Y-m-d') !== self::INITIAL_IMPORT_DATE && $bDate->format('Y-m-d') === self::INITIAL_IMPORT_DATE) {
+                            return $sorting->getSortDirection() === SortDirection::ASC ? 1 : -1;
+                        }
+
+                        if ($aDate->format('Y-m-d') === self::INITIAL_IMPORT_DATE && $bDate->format('Y-m-d') === self::INITIAL_IMPORT_DATE) {
+                            // We will take the ID into account here.
+                            if ($a->getId() === $b->getId()) {
+                                return 0;
+                            }
+
+                            if ($sorting->getSortDirection() === SortDirection::ASC) {
+                                return ($a->getId() < $b->getId()) ? -1 : 1;
+                            }
+
+                            return ($a->getId() > $b->getId()) ? -1 : 1;
+                        }
+
+                        // Starting from here it's just normal sorting on date.
+                        $aDateValue = strtotime($aDate->format('Y-m-d H:i:s'));
+                        $bDateValue = strtotime($bDate->format('Y-m-d H:i:s'));
+                        if ($sorting->getSortDirection() === SortDirection::ASC) {
+                            return ($aDateValue < $bDateValue) ? -1 : 1;
+                        }
+
+                        return ($aDateValue > $bDateValue) ? -1 : 1;
                     }
+                );
+                break;
+            default:
+                usort(
+                    $this->rows,
+                    function (Row $a, Row $b) use ($sorting) {
+                        $sortField = $sorting->getSortField();
 
-                    return ($aDateValue > $bDateValue) ? -1 : 1;
-                }
-            );
-
-            return;
+                        if ($sorting->getSortDirection() === SortDirection::ASC) {
+                            return strcmp($a->getValueForSortField($sortField), $b->getValueForSortField($sortField));
+                        }
+                        return strcmp($b->getValueForSortField($sortField), $a->getValueForSortField($sortField));
+                    }
+                );
         }
-
-        usort(
-            $this->rows,
-            function (Row $a, Row $b) use ($sorting) {
-                $sortField = $sorting->getSortField();
-
-                if ($sorting->getSortDirection() === SortDirection::ASC) {
-                    return strcmp($a->getValueForSortField($sortField), $b->getValueForSortField($sortField));
-                }
-                return strcmp($b->getValueForSortField($sortField), $a->getValueForSortField($sortField));
-            }
-        );
     }
 
     /**
--- src/Sort/SortField.php
+++ src/Sort/SortField.php
@@ -4,9 +4,6 @@ namespace App\Sort;
 
 enum SortField: string
 {
-    public const TYPE_NATURAL = 'natural';
-    public const TYPE_NUMERIC = 'numeric';
-    public const TYPE_DATE = 'date';
 
     case DATE = 'date';
     case TITLE = 'title';
@@ -18,14 +15,5 @@ enum SortField: string
     public function toUpper(): string
     {
         return strtoupper($this->value);
-    }
-
-    public function getType(): string
-    {
-        return match ($this) {
-            SortField::TITLE => self::TYPE_NATURAL,
-            SortField::DATE => self::TYPE_DATE,
-            default => self::TYPE_NUMERIC,
-        };
     }
 }
