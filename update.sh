04d06206535c2bdb3d3154a94f5390fb62eac335
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2023-04-17 18:27:21 +0200

    better script

--- update.sh
+++ update.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+git pull
+
 COMMIT_MESSAGE=$(php bin/easy-platinums games:fetch caro3c-gabber9,Fluttezuhher,chris_play1,IBadDriverI,roots52,The-Ricksterr,Hakoom,ikemenzi)
 
 if [[ -z "$COMMIT_MESSAGE" ]]
